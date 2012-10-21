e3e93ef35ec8dfbf1bb05c41dc59763f74de4072
ajlopez <webmaster@ajlopez.com>
2012-10-21 19:24:48 -0300

    States changes can be specified by a function, with test

--- test/linear.js
+++ test/linear.js
@@ -57,3 +57,24 @@ for (var k = 0; k < 15; k++) {
         assert.equal(value, 0);
 }
 
+// rule 126 by function: 2 or 1 alive neighbours then alive
+
+var automata = cellular.createLinearAutomata(15, 2, 1, function(values) {
+    var total = 0;
+    for (var k = 0; k < 3; k++)
+        total += values[k];
+    if (total == 1 || total == 2)
+        return 1;
+    return 0;
+});
+
+automata.setCellValue(7, 1);
+automata.nextGeneration();
+
+for (var k = 0; k < 15; k++) {
+    var value = automata.getCellValue(k);
+    if (k >= 6 && k <= 8)
+        assert.equal(value, 1);
+    else
+        assert.equal(value, 0);
+}
